7cf11e0ea4a6302c60bc3f59c37aca0593ce80ee
PrakharSuraj <prakharsurajwipro@gmail.com>
2025-02-03 16:04:40 +0530

    Day-2 Java

--- wipro_java_project/src/com/wipro/oops/abstraction/Car.java
+++ wipro_java_project/src/com/wipro/oops/abstraction/Car.java
@@ -3,7 +3,7 @@ package com.wipro.oops.abstraction;
 //  Abstract class enforcing abstraction
 public abstract class Car {
 
-    //  Abstract methods 
+    //  Abstract methods  for mileage and speed
     public abstract void mileage();
     public abstract void speed();
 }
