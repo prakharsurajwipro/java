4a0618213312440cf6e8fbc9369ae183fde3a013
PrakharSuraj <prakharsurajwipro@gmail.com>
2025-02-07 11:50:18 +0530

    Day-5 Java

---
+++ wipro_java_project/src/com/wipro/java/java8/StringApi.java
@@ -0,0 +1,24 @@
+package com.wipro.java.java8;
+
+public class StringApi {
+
+	public StringApi() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		String str="Prakhar Suraj";
+		System.out.println(str.length());
+		System.out.println(str.charAt(2));
+		System.out.println(str.substring(1,3));
+		System.out.println(str.contains("a"));
+		String[] a=str.split("");
+		
+		for(String str1:a)
+		{
+			System.out.println(str1);
+		}
+		
+	}
+
+}
