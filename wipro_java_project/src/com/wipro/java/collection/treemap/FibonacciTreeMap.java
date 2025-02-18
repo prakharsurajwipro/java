1f82cf373d6bd464842b6a321783ed9934f3b6e0
prakharsurajwipro <prakharsurajwipro@gmail.com>
2025-02-18 18:06:44 +0530

    commit

---
+++ wipro_java_project/src/com/wipro/java/collection/treemap/FibonacciTreeMap.java
@@ -0,0 +1,32 @@
+package com.wipro.java.collection.treemap;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class FibonacciTreeMap {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of Fibonacci terms: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        TreeMap<Integer, Long> fibonacciMap = new TreeMap<>();
+        generateFibonacci(n, fibonacciMap);
+
+        System.out.println("Fibonacci Series stored in TreeMap:");
+        for (Map.Entry<Integer, Long> entry : fibonacciMap.entrySet()) {
+            System.out.println("Term " + entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+
+    private static void generateFibonacci(int n, TreeMap<Integer, Long> fibonacciMap) {
+        long first = 0, second = 1;
+        for (int i = 1; i <= n; i++) {
+            fibonacciMap.put(i, first);
+            long next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
